82c4d3b94e62e87659f6f7c2328b4a962f8260db
dasimems <isaacseun63@gmail.com>
2023-08-06 23:39:14 +0100

    update: updated the image

--- assets/images/index.js
+++ assets/images/index.js
@@ -1,51 +1,6 @@
-import logoImage from "./logo.png";
 import loadingImage from "./loading.gif";
 import MaleAvatarOne from "./avatar_male.png";
-import MaleAvatarTwo from "./avatar_male_1.png";
-import MaleAvatarThree from "./avatar_male_2.png";
-import FemaleAvatarOne from "./avatar_female.png";
-import OnboardingBackgroundOne from "./onboarding-one.png";
-import OnboardingTwoImage from "./onboarding-two-image.png";
-import OnboardingOneImage from "./onboarding-one-image.png";
-import RegistrationImage from "./registration-image.png";
-import FemaleAvatarTwo from "./avatar_female_1.png";
-import Bike from "./bike.png";
-import Car from "./car.png";
-import Van from "./van.png";
-import Truck from "./truck.png";
-import GettingStartedImageOne from "./getting-started-1.jpg";
-import GettingStartedImageTwo from "./getting-started-2.jpg";
-import GettingStartedImageThree from "./getting-started-3.jpg";
-import OpayLogo from "./opay.png";
-import PalmpayLogo from "./palmpay.png";
-import PalmpayBannerOne from "./palmpay-banner-1.jpg";
 import LoaderImage from "./loader.gif";
-import WithdrawalImage from "./withdrawal-image.jpg";
-import GettingStartedImage from "./getting-started-1.png";
+import GettingStartedImage from "./new-getting-started.png";
 
-export {
-  logoImage,
-  loadingImage,
-  MaleAvatarOne,
-  MaleAvatarTwo,
-  MaleAvatarThree,
-  FemaleAvatarTwo,
-  FemaleAvatarOne,
-  OnboardingBackgroundOne,
-  OnboardingOneImage,
-  OnboardingTwoImage,
-  RegistrationImage,
-  Bike,
-  Car,
-  Van,
-  Truck,
-  GettingStartedImageOne,
-  GettingStartedImageTwo,
-  GettingStartedImageThree,
-  OpayLogo,
-  PalmpayLogo,
-  PalmpayBannerOne,
-  LoaderImage,
-  WithdrawalImage,
-  GettingStartedImage
-};
+export { loadingImage, MaleAvatarOne, LoaderImage, GettingStartedImage };
